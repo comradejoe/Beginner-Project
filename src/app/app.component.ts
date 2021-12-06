58fbb2a753305a08e10d5a0098a4085cf400fda8
comradejoe <joejeeva14@gmail.com>
2021-12-06 15:38:53 +0530

    save as pdf added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { StudentsService } from "./students.service";
-
+import { jsPDF } from "jspdf";
+import html2canvas from "html2canvas";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
@@ -50,4 +51,19 @@ export class AppComponent {
       });
     });
   }
+  print() {
+    let data = document.getElementById("printPage");
+    this.generatePdf(data);
+  }
+  generatePdf(data) {
+    html2canvas(data).then((canvas) => {
+      let imgWidth = 290;
+      let imgHeight = (canvas.height * imgWidth) / canvas.width;
+      const contentDataURL = canvas.toDataURL("image/png");
+      let pdf = new jsPDF("l", "mm", "a4");
+      var position = 10;
+      pdf.addImage(contentDataURL, "PNG", 0, position, imgWidth, imgHeight);
+      pdf.save("StudentsData.pdf");
+    });
+}
 }
